4d6516fdd4ae1eaee6f1dc281a0e12c0c8c57300
BearingMe <gomes.bruno201@gmail.com>
2026-03-14 16:35:09 -0300

    chore: add Vue Query and Zod dependencies, configure Vue Query plugin

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
+<script setup>
+import { VueQueryDevtools } from "@tanstack/vue-query-devtools";
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
+  <VueQueryDevtools />
 </template>
