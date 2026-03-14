0e6f1ef01c66d5a9bca16154dad4692c75e36b46
BearingMe <gomes.bruno201@gmail.com>
2026-03-14 17:25:06 -0300

    chore: update ESLint configuration to include custom rules for Vue and TypeScript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,14 @@
 import withNuxt from "./.nuxt/eslint.config.mjs";
 import prettierConfig from "eslint-config-prettier";
 
-export default withNuxt(prettierConfig);
+export default withNuxt(
+  {
+    rules: {
+      "vue/multi-word-component-names": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "vue/block-order": ["error", { order: ["script", "template", "style"] }],
+      "vue/block-lang": ["error", { script: { lang: "ts" } }],
+    },
+  },
+  prettierConfig,
+);
